f7e4c21dbf2a44f7c2544ed4e9cb74e9223973dd
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2023-09-03 16:34:22 +0700

    initial project

---
+++ app/services/database.js
@@ -0,0 +1,14 @@
+const db = require('mysql2');
+require('dotenv').config();
+
+const connection = db.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
