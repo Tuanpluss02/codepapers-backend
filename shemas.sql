4e5d4e28efbb92abecb42358bcdc0998ac3a3dbb
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2023-09-04 12:08:07 +0700

    (chore) add schemas.sql

---
+++ shemas.sql
@@ -0,0 +1,28 @@
+
+drop table if exists users, posts; 
+
+create table users
+(
+    id             int auto_increment
+        primary key,
+    email          varchar(255) not null,
+    password       varchar(255) not null,
+    fullName      varchar(255) not null,
+    profileAvatar varchar(255) null,
+    dateOfBirth  date         null
+);
+
+create table posts
+(
+    id      int auto_increment
+        primary key,
+    title   varchar(255) not null,
+    body    text       not null ,
+    postedAt datetime not null,
+    userId int         not null,
+    constraint posts_ibfk_1
+        foreign key (userId) references users (id)
+);
+
+create index userId
+    on posts (userId);
