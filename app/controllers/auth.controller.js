f8ebea70376e73d696048d42337951c6114b698d
dannguyenmessi1705 <messiprohy@gmail.com>
2023-09-05 15:16:53 +0700

    Pretty Code Register

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,6 +1,5 @@
 const HTTPStatusCode = new (require("../common/constants/HttpStatusCode.js"))();
 const query = require("../modules/user.query.js");
-const db = require("../services/database.service.js");
 const authServices = require("../services/auth.service.js");
 const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
@@ -17,57 +16,46 @@ exports.auth = {
       user: user,
     });
   },
-  register: (req, res) => {
+  register: async (req, res) => {
     const email = req.body.email;
     const name = req.body.name;
     const password = req.body.password;
     const profileAvatar = req.body.profileAvatar;
     const dateOfBirth = req.body.dateOfBirth;
-    db.query(
-      "SELECT * FROM users WHERE email = ?;",
-      [email],
-      async (err, result) => {
-        if (err) {
-          console.error(err);
-          return res.status(HTTPStatusCode.InternalServerError).json({
-            message: "Register failed",
-          });
-        }
-        if (result.length > 0) {
-          return res.status(HTTPStatusCode.BadRequest).json({
-            message: "Email already exists",
-          });
-        }
-        const hashedPassword = await authServices.hashPassword(password);
-        db.query(
-          "INSERT INTO users (fullName, email, password, profileAvatar, dateOfBirth) VALUES (?, ?, ?, ?, ?);",
-          [
-            name,
-            email,
-            hashedPassword,
-            profileAvatar,
-            dateOfBirth,
-          ],
-          (err, result) => {
-            if (err) {
-              console.error(err);
-              return res.status(HTTPStatusCode.InternalServerError).json({
-                message: "Register failed",
-              });
-            }
-            console.log(result);
-            const accessToken = authServices.generateToken(
-              { _id: uuidv4() },
-              process.env.ACCESS_TOKEN_SECRET
-            );
-            return res.status(HTTPStatusCode.OK).json({
-              message: "Register successful",
-              accessToken,
-            });
-          }
-        );
+    try {
+      const userList = await query.getUsers(email);
+      if (userList.length > 0) {
+        return res.status(HTTPStatusCode.BadRequest).json({
+          message: "Email already exists",
+        });
       }
-    );
+      const hashedPassword = await authServices.hashPassword(password);
+      const createUser = await query.createUser(
+        name,
+        email,
+        hashedPassword,
+        profileAvatar,
+        dateOfBirth
+      );
+      if (!createUser) {
+        return res.status(HTTPStatusCode.InternalServerError).json({
+          message: "Register failed",
+        });
+      }
+      const accessToken = authServices.generateToken(
+        { _id: uuidv4() },
+        process.env.ACCESS_TOKEN_SECRET
+      );
+      return res.status(HTTPStatusCode.OK).json({
+        message: "Register successful",
+        accessToken,
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(HTTPStatusCode.InternalServerError).json({
+        message: "Register failed",
+      });
+    }
   },
   verify: (req, res) => {
     res.status(HTTPStatusCode.OK).json({
