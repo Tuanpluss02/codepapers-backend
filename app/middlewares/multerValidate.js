6c40fd5de54a4561386fecc2213d47b4413188cc
Tuanpluss02 <dongoctuan2002sp@gmail.com>
2023-11-02 19:54:53 +0700

    (chore) remove trash file

--- app/middlewares/multerValidate.js
+++
@@ -1,4 +0,0 @@
-const multer = require("multer");
-const { diskStorage, fileFilter } = require("../utils/multerConfig");
-
-exports.multerProcess = multer({ storage: diskStorage, fileFilter: fileFilter }).single("avatar");
