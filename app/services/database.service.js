ebe9ac4fa5becfe8c91b61124b4720d56a47e131
dannguyenmessi1705 <messiprohy@gmail.com>
2023-11-06 11:05:10 +0700

    DATABASE ONLINE

--- app/services/database.service.js
+++ app/services/database.service.js
@@ -1,16 +1,22 @@
 const db = require("mysql2");
 require("dotenv").config();
 
+// connect mysql in digital ocean
 const conn = db.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  dateStrings: true,
+  port: process.env.DB_PORT
 });
 
 conn.connect((err) => {
-  if (err) throw err;
+  if (err) {
+    console.log("Error connecting to Db");
+    return;
+  }
   console.log(`✅ Connected to database: ${process.env.DB_NAME}`);
 });
+
+
 module.exports = conn;
